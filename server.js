6a0f2424091adedf5488a37706fe10b9eb31d9e0
wangqin <wangqin@zhongan.com>
2018-03-02 12:58:18 +0800

    src/fetch.js

--- server.js
+++ server.js
@@ -13,47 +13,45 @@ const app = new Koa();
 const router = new Router();
 
 app.use(cors({
-    credentials: true
+  credentials: true
 }));
 
-router.post('/index',async function (ctx, next) {
-    console.log('request body ======>', ctx.request.body);
-    const result = await new Promise(function(resolve){
-        const file = ctx.request.body.files.file;
-        const ext = file.name.replace(/.+(?=\.[^.]+$)/, '');
-        const storePath =path.resolve(__dirname, './upload/' + Date.now() + ext);
-        const stream = fs.createWriteStream(storePath);//创建一个可写流
-        fs.createReadStream(file.path).pipe(stream).on('close', () => {
-            resolve({
-                status: 200,
-                body: {success: true},
-            });
-       }).on('error', (err) => {
-            resolve({
-                status: 500,
-                body: {success: false, errorMsg: err},
-            });
-        });//可读流通过管道写入可写流
-    });
+router.post('/index', async (ctx) => {
+  console.log('request body ======>', ctx.request.body);
+  const result = await new Promise(((resolve) => {
+    const file = ctx.request.body.files.file;
+    const ext = file.name.replace(/.+(?=\.[^.]+$)/, '');
+    const storePath = path.resolve(__dirname, `./upload/${Date.now()}${ext}`);
+    const stream = fs.createWriteStream(storePath);// 创建一个可写流
+    fs.createReadStream(file.path).pipe(stream).on('close', () => {
+      resolve({
+        status: 200,
+        body: { success: true }
+      });
+    }).on('error', (err) => {
+      resolve({
+        status: 500,
+        body: { success: false, errorMsg: err }
+      });
+    });// 可读流通过管道写入可写流
+  }));
 
-    ctx.status = result.status;
-    ctx.body = result.body;
+  ctx.status = result.status;
+  ctx.body = result.body;
 });
 
 async function doFetch() {
-    const result = await request({
-        url: 'https://www.baidu.com',
-        method: 'post',
-        translateResponseBySelf: true,
-        body: {a: Date.now()}
-    }).then((res)=>{
-        return res.text();
-    });
-    console.log('result ------>', result.slice(0,17));
-};
+  const result = await request({
+    url: 'https://www.baidu.com',
+    method: 'post',
+    translateResponseBySelf: true,
+    body: { a: Date.now() }
+  }).then(res => res.text());
+  console.log('result ------>', result.slice(0, 17));
+}
 
 doFetch();
 
-app.use(koaBody({multipart: true})).use(bodyParser()).use(router.routes()).use(router.allowedMethods());
+app.use(koaBody({ multipart: true })).use(bodyParser()).use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000, ()=>console.log('Koa start at 3000...'));
+app.listen(3000, () => console.log('Koa start at 3000...'));
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import request from './fetch'
+import request from './fetch';
 
 async function doFetch(data) {
-    const result = await request({
-        url: 'http://localhost:3000/index',
-        method: 'post',
-        errorTips: true,
-        body: data
-    });
-    console.log('result:', result);
-    if(result && result.success){
-        alert('ok!')
-    }
-};
-
-document.getElementsByTagName('input')[0].onchange = function(){
-    const data= new FormData();
-    data.append('name', 'FormData');
-    data.append('file', this.files[0]);
-    doFetch(data);
+  const result = await request({
+    url: 'http://localhost:3000/index',
+    method: 'post',
+    errorTips: true,
+    body: data
+  });
+  console.log('result:', result);
+  if (result && result.success) {
+    alert('ok!');
+  }
 }
+
+document.getElementsByTagName('input')[0].onchange = function () {
+  const data = new FormData();
+  data.append('name', 'FormData');
+  data.append('file', this.files[0]);
+  doFetch(data);
+};
